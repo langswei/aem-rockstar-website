644552a8c20b61e82669ae30cd22185706968d0d
Sean Steimer <ssteimer@adobe.com>
2022-07-20 15:31:06 -0700

    added license header to carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,14 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
 
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 function loadGlider() {
     const gliderScript = document.createElement('script');
@@ -14,7 +24,7 @@ function loadGlider() {
 export default function decorate(block) {
     loadGlider();
 
-    //decorate carousel with stuff we want
+    //decorate carousel with controls
     const prev = document.createElement('button');
     prev.classList.add('glider-prev');
     prev.innerText = '<'
